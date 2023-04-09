1f480e0c470d347887a34e388d696adb33209c6b
cpereira42 <cpereira@42sp.org.br>
2023-04-09 10:42:29 -0300

    adding getters and setteron ConfigServer

--- headers/ConfigServer.hpp
+++ headers/ConfigServer.hpp
@@ -8,7 +8,24 @@
 
 class ConfigServer {
     public :
+        ConfigServer (void);
+        ConfigServer (std::string fileName);
+        ConfigServer (const ConfigServer &obj);
+        ~ConfigServer (void);
+
+        ConfigServer& operator=(const ConfigServer& obj);
         
+        // Setters
+        void setServeName(std::string serverName);
+        void setErrorPages(int code, std::string page);
+        void setPorts(int port);
+        void setClientMaxBodySize(int clientMaxBodySize);
+
+        // Getters
+        std::string                 getServerName(void);
+        std::map<int, std::string>  getErrorPorts(void);
+        std::set<int>               getPorts(void);
+        int                         getClientMaxBodySize(void);
 
     private :
         std::string                 _serverName;
