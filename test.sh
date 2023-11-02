6e68f7ad0f51b43a02dae5759cfc48cdca435075
cpereira42 <cpereira@student.42sp.org.br>
2023-11-02 19:49:49 -0300

    adicionado testes

--- test.sh
+++ test.sh
@@ -4,30 +4,12 @@ URL="www.localhost:8080"
 URL2="www.localhost:8080/99"
 
 URLIMG="http://localhost:8081/upload"
-
-
-curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/rg_frente.jpeg" > response_code.txt
-HTTP_STATUS=$(cat response_code.txt)
-if [ "$HTTP_STATUS" = "404" ]; then
-    echo "A solicitação foi bem-sucedida (código de status 404)."
-else
-    echo "A solicitação falhou (código de status $HTTP_STATUS)."
-fi
-rm response_code.txt
-
+URLDELTE="http://localhost:8081/delete.html"
 
 
 #curl -v www.localhost:$PORT >> response_code.txt
 
 ###### GET ########
-
-IMAGE_FILE="1234.png"
-
-#curl -X POST -F @1234.png http://localhost:8081/upload.html
-
-curl -X POST -F "imagem=@$IMAGE_FILE" $URLIMG
-
-#curl -v $URL
 
 #### get de uma pagina valida
 curl -s -o /dev/null -w "%{http_code}" $URL > response_code.txt
@@ -44,7 +26,7 @@ rm response_code.txt
 curl -s -o /dev/null -w "%{http_code}" $URL2 > response_code.txt
 HTTP_STATUS=$(cat response_code.txt)
 if [ "$HTTP_STATUS" = "404" ]; then
-    echo "A solicitação foi bem-sucedida (código de status 404)."
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
 else
     echo "A solicitação falhou (código de status $HTTP_STATUS)."
 fi
@@ -52,14 +34,60 @@ rm response_code.txt
 
 
 ### get de uma imagem invalida
-curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/rg_frente.jpeg" > response_code.txt
+curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/produto.png" > response_code.txt
 HTTP_STATUS=$(cat response_code.txt)
 if [ "$HTTP_STATUS" = "404" ]; then
-    echo "A solicitação foi bem-sucedida (código de status 404)."
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
 else
     echo "A solicitação falhou (código de status $HTTP_STATUS)."
 fi
 rm response_code.txt
+
+### post envio de arquivo
+IMAGE_FILE="produto.png"
+curl -s -o /dev/null -X POST -w "%{http_code}" -F "imagem=@$IMAGE_FILE" $URLIMG > response_code.txt
+HTTP_STATUS=$(cat response_code.txt)
+if [ "$HTTP_STATUS" = "301" ]; then
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
+else
+    echo "A solicitação falhou (código de status $HTTP_STATUS)."
+fi
+
+### get de uma imagem valida
+curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/produto.png" > response_code.txt
+HTTP_STATUS=$(cat response_code.txt)
+if [ "$HTTP_STATUS" = "200" ]; then
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
+else
+    echo "A solicitação falhou (código de status $HTTP_STATUS)."
+fi
+rm response_code.txt
+
+### DELETA o arquivo enviado
+curl -s -o /dev/null -X DELETE -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/produto.png" > response_code.txt
+HTTP_STATUS=$(cat response_code.txt)
+if [ "$HTTP_STATUS" = "200" ]; then
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
+else
+    echo "A solicitação falhou (código de status $HTTP_STATUS)."
+fi
+
+rm response_code.txt
+
+### DELETA o arquivo enviado
+curl -s -o /dev/null -X DELETE -w "%{http_code}" "http://localhost:8081/pages/site2/uploads/produto.png" > response_code.txt
+HTTP_STATUS=$(cat response_code.txt)
+if [ "$HTTP_STATUS" = "200" ]; then
+    echo "A solicitação foi bem-sucedida (código de status $HTTP_STATUS)."
+else
+    echo "A solicitação falhou (código de status $HTTP_STATUS)."
+fi
+
+rm response_code.txt
+
+
+
+
 
 
 
