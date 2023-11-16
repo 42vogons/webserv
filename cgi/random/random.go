537d054fac487f7074bd1995154f09d24298fb7e
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2023-11-16 01:07:35 -0300

    random to english

--- cgi/random/random.go
+++ cgi/random/random.go
@@ -7,54 +7,45 @@ import (
 	"net/http"
 )
 
-// Fato representa a estrutura JSON da resposta da API
-type Fato struct {
+type Source struct {
 	ID        string `json:"id"`
-	Texto     string `json:"text"`
-	Fonte     string `json:"source"`
-	URLFonte  string `json:"source_url"`
-	Idioma    string `json:"language"`
+	Text      string `json:"text"`
+	Source    string `json:"source"`
+	SourceURL string `json:"source_url"`
+	Language  string `json:"language"`
 	Permalink string `json:"permalink"`
 }
 
 func main() {
-	// Definindo o cabeçalho de conteúdo
 	fmt.Println("Content-Type: text/html; charset=utf-8")
 	fmt.Println()
-
-	fmt.Println("<html><head><title>Fato Aleatório</title></head><body>")
-	fmt.Println("<h1>Fato Aleatório</h1>")
-
-	// Chamando a API para obter um fato aleatório
-	fato, err := obterFatoAleatorio()
+	fmt.Println("<html><head><title>Useless Fact</title></head><body>")
+	fmt.Println("<h1>Useless Fact</h1>")
+	source, err := getSource()
 	if err != nil {
-		fmt.Println("<p>Erro ao obter o fato: ", err, "</p>")
+		fmt.Println("<p>error getting useless fact: ",err,"</p>")
 	} else {
-		fmt.Println("<p>", fato.Texto, "</p>")
-		fmt.Println("<p><a href='", fato.URLFonte, "'>Fonte</a></p>")
+		fmt.Println("<p>",  source.Text,"</p>")
+		fmt.Println("<p><a href='", source.SourceURL,"'>Font</a></p>")
 	}
-
 	fmt.Println("</body></html>")
 }
 
-func obterFatoAleatorio() (*Fato, error) {
+func getSource() (*Source, error) {
 	url := "https://uselessfacts.jsph.pl/api/v2/facts/random?language=%22pt-BR%22"
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
-
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
-
-	var fato Fato
-	err = json.Unmarshal(body, &fato)
+	var source Source
+	err = json.Unmarshal(body, &source)
 	if err != nil {
 		return nil, err
 	}
-
-	return &fato, nil
+	return &source, nil
 }
